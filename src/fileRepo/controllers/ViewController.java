3e3f65d040c47a79982c676db531532604c667c0
anh23 <anh23@rambler.ru>
2019-02-03 20:40:29 +0300

    Infrastructure, Base page

---
+++ src/fileRepo/controllers/ViewController.java
@@ -0,0 +1,19 @@
+package fileRepo.controllers;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+//@SessionAttributes("roles")
+public class ViewController {
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public ModelAndView Home() {
+		ModelAndView mav = new ModelAndView("/WEB-INF/index.jsp");
+        return mav;
+	}
+}
